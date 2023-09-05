3c9967e6cfb6ec7b5d84a408e64f5ba831f6a43e
Andrej Kmetík <akmetik@gmail.com>
2023-09-05 17:27:25 +0200

    BacnetTimeValue: Add support for Values with BACNET_APPLICATION_TAG_NULL.

--- DataUnits/BacnetTimeValue.cs
+++ DataUnits/BacnetTimeValue.cs
@@ -9,12 +9,13 @@ BACnetTimeValue ::= SEQUENCE {
 public struct BacnetTimeValue : ASN1.IEncode, ASN1.IDecode
 {
     public BacnetTime Time { get; set; }
-    public uint Value { get; set; }
+    public uint? Value { get; set; }
 
     public void Encode(EncodeBuffer buffer)
     {
-        Time.Encode(buffer);
-        ASN1.encode_application_unsigned(buffer, Value);
+        //Time.Encode(buffer);
+        //ASN1.encode_application_unsigned(buffer, Value);
+        throw new NotImplementedException();
     }
 
     public int Decode(byte[] buffer, int offset, uint count)
@@ -27,9 +28,20 @@ public struct BacnetTimeValue : ASN1.IEncode, ASN1.IDecode
         // TODO
         // This can be any primitive type - not only unsigned
         len += ASN1.decode_tag_number_and_value(buffer, offset + len, out tagNumber, out var lenValueType);
-        len += ASN1.decode_unsigned(buffer, offset + len, lenValueType, out var value);
+        if (tagNumber == (byte)BacnetApplicationTags.BACNET_APPLICATION_TAG_NULL)
+        {
+            Value = null;
+        }
+        else if (tagNumber == (byte)BacnetApplicationTags.BACNET_APPLICATION_TAG_UNSIGNED_INT || tagNumber == (byte)BacnetApplicationTags.BACNET_APPLICATION_TAG_ENUMERATED)
+        {
+            len += ASN1.decode_unsigned(buffer, offset + len, lenValueType, out var value);
+            Value = value;
+        }
+        else
+        {
+            throw new NotImplementedException($"Decode for tag number {tagNumber} is not implemented.");
+        }
         
-        Value = value;
         return len;
     }
 
