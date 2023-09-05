5bbf9b4d250490098f0824f81d36b011287f8d21
Andrej Kmetík <akmetik@gmail.com>
2023-09-05 16:42:12 +0200

    Rewrite BACnetCalendarEntry in-line with the standard.

--- Base/BaCnetCalendarEntry.cs
+++ Base/BaCnetCalendarEntry.cs
@@ -1,69 +1,113 @@
 ﻿namespace System.IO.BACnet;
 
-public struct BACnetCalendarEntry : ASN1.IEncode, ASN1.IDecode
+/*
+BACnetCalendarEntry ::= CHOICE {
+ date[0] Date,
+ dateRange[1] BACnetDateRange,
+ weekNDay[2] BACnetWeekNDay
+ }
+*/
+public class BacnetCalendarEntry : ASN1.IEncode, ASN1.IDecode
 {
-    public List<object> Entries; // BacnetDate or BacnetDateRange or BacnetweekNDay
+    public BacnetDate? Date { get; set; }
+    public BacnetDateRange? DateRange { get; set; }
+    public BacnetWeekNDay? WeekNDay { get; set; }
+
+    public BacnetCalendarEntry()
+    {
+        
+    }
+
+    public BacnetCalendarEntry(BacnetDate date)
+    {
+        Date = date;
+    }
+
+    public BacnetCalendarEntry(BacnetDateRange dateRange)
+    {
+        DateRange = dateRange;
+    }
+
+    public BacnetCalendarEntry(BacnetWeekNDay weekNDay)
+    {
+        WeekNDay = weekNDay;
+    }
 
     public void Encode(EncodeBuffer buffer)
     {
-        if (Entries == null)
-            return;
-
-        foreach (ASN1.IEncode entry in Entries)
+        if (Date is not null)
         {
-            if (entry is BacnetDate)
-            {
-                ASN1.encode_tag(buffer, 0, true, 4);
-                entry.Encode(buffer);
-            }
-
-            if (entry is BacnetDateRange)
-            {
-                ASN1.encode_opening_tag(buffer, 1);
-                entry.Encode(buffer);
-                ASN1.encode_closing_tag(buffer, 1);
-            }
-
-            if (entry is BacnetWeekNDay)
-            {
-                ASN1.encode_tag(buffer, 2, true, 3);
-                entry.Encode(buffer);
-            }
+            ASN1.encode_tag(buffer, 0, true, 4);
+            Date.Value.Encode(buffer);
+            return;
         }
+
+        if (DateRange is not null)
+        {
+            ASN1.encode_opening_tag(buffer, 1);
+            DateRange.Value.Encode(buffer);
+            ASN1.encode_closing_tag(buffer, 1);
+            return;
+        }
+
+        if (WeekNDay is not null)
+        {
+            ASN1.encode_tag(buffer, 2, true, 3);
+            WeekNDay.Value.Encode(buffer);
+            return;
+        }
+
+        throw new Exception($"Invalid {nameof(BacnetCalendarEntry)}");
     }
 
     public int Decode(byte[] buffer, int offset, uint count)
     {
         var len = 0;
 
-        Entries = new List<object>();
+        len += ASN1.decode_tag_number(buffer, offset, out byte tagNumber);
 
-        while (true)
+        switch (tagNumber)
         {
-            len += ASN1.decode_tag_number(buffer, offset + len, out byte tagNumber);
-
-            switch (tagNumber)
-            {
-                case 0:
-                    var bdt = new BacnetDate();
-                    len += bdt.Decode(buffer, offset + len, count);
-                    Entries.Add(bdt);
-                    break;
-                case 1:
-                    var bdr = new BacnetDateRange();
-                    len += bdr.Decode(buffer, offset + len, count);
-                    Entries.Add(bdr);
-                    len++; // closing tag
-                    break;
-                case 2:
-                    var bwd = new BacnetWeekNDay();
-                    len += bwd.Decode(buffer, offset + len, count);
-                    Entries.Add(bwd);
-                    break;
-                default:
-                    return len - 1; // closing Tag
-            }
+            case 0:
+                var bdt = new BacnetDate();
+                len += bdt.Decode(buffer, offset + len, count);
+                Date = bdt;
+                // Date does NOT have a closing tag
+                break;
+            case 1:
+                var bdr = new BacnetDateRange();
+                len += bdr.Decode(buffer, offset + len, count);
+                DateRange = bdr;
+                len++; // Date Range has a closing tag
+                break;
+            case 2:
+                var bwd = new BacnetWeekNDay();
+                len += bwd.Decode(buffer, offset + len, count);
+                WeekNDay = bwd;
+                // Date does NOT have a closing tag
+                break;
         }
 
+        return len + 1; // Closing tag of BacnetCalendarEntry
+    }
+
+    public override string ToString()
+    {
+        if (Date != null)
+        {
+            return Date.ToString();
+        }
+
+        if (DateRange != null)
+        {
+            return DateRange.ToString();
+        }
+
+        if (WeekNDay != null)
+        {
+            return WeekNDay.ToString();
+        }
+
+        return base.ToString();
     }
 }
