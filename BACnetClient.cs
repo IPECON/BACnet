0a777f47919d8858fd44f4d72368a520d3a4c59c
Andrej Kmetík <akmetik@gmail.com>
2025-06-11 10:35:46 +0200

    GetAlarmSummary: Add inner exception when decode fails.

--- BACnetClient.cs
+++ BACnetClient.cs
@@ -2337,7 +2337,7 @@ public class BacnetClient : IBacnetMessageFactoryParameters, IDisposable
         }
         else
         {
-            ex = new Exception("Service not available");
+            ex = new Exception("Service not available", ex);
         }
 
         res.Dispose();
