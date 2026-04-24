ae761a290900e33bebb88ef9d4d2d5a709e132cc
Andrej Kmetík <akmetik@gmail.com>
2026-04-24 12:11:41 +0200

    Rework processing of segments (received)
    - Allow segments to be received out of order and correctly acknowledge them
    - Check if segments received are continuous before sending segmentACK
    - Ignore segments if they were received more than once

---
+++ Helpers/SegmentationHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace System.IO.BACnet.Helpers
+{
+    public class SegmentationHelper
+    {
+        private readonly Dictionary<byte, SegmentationInfo> _invokeIdToInfoMap = new();
+
+        public void Clear(byte invokeId)
+        {
+            _invokeIdToInfoMap.Remove(invokeId);
+        }
+
+        public SegmentationInfo GetSegmentationInfo(byte invokeId, bool createNew)
+        {
+            if (createNew)
+            {
+                var newInfo = new SegmentationInfo();
+                _invokeIdToInfoMap[invokeId] = newInfo;
+                return newInfo;
+            }
+
+            if (!_invokeIdToInfoMap.TryGetValue(invokeId, out var info))
+            {
+                info = new SegmentationInfo();
+                _invokeIdToInfoMap[invokeId] = info;
+            }
+
+            return info;
+        }
+    }
+}
